3acd59e009ca46609b2a2d6bf892825efd03d2ab
qiaoyacong <44867841+qiaoyacong@users.noreply.github.com>
2018-11-08 20:26:55 +0800

    modules

---
+++ mores/mores.js
@@ -0,0 +1,74 @@
+// pages/mores/mores.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    citylist:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: 'http://127.0.0.1:3000/cityintro',
+      success:(res)=>{
+        console.log(res.data);
+        this.setData({
+          citylist:res.data
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
